bcacd06e3ef2cc6fbbf7c955475adbb641ac5bf8
YeonHee Kim <kyh03179@gmail.com>
2024-09-05 09:37:43 +0900

    [TEST] gRPC 통신 테스트 완료

--- src/main/java/com/yonyk/talaria/resources/TalariaResourcesApplication.java
+++ src/main/java/com/yonyk/talaria/resources/TalariaResourcesApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TalariaResourcesApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TalariaResourcesApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(TalariaResourcesApplication.class, args);
+  }
 }
