42e152fb9aaeef7eca445e52ffe19c67b1c98e8c
YeonHee Kim <kyh03179@gmail.com>
2024-09-05 09:20:39 +0900

    [SETTING] 스켈레톤 프로젝트 생성

---
+++ src/main/java/com/yonyk/talaria/resources/TalariaResourcesApplication.java
@@ -0,0 +1,13 @@
+package com.yonyk.talaria.resources;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalariaResourcesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalariaResourcesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yonyk/talaria/resources/TalariaResourcesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yonyk.talaria.resources;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalariaResourcesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
